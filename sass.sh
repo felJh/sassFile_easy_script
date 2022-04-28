a15c5f610e7760b4b79b45761839661a4308727b
felJh <fel.jhoe@gmail.com>
2022-04-28 21:12:49 +0200

    npm > sass installed

--- sass.sh
+++ sass.sh
@@ -7,16 +7,14 @@ mkdir -p public/{css,js,img,fonts}
 sleep 1
 
 touch index.html
-
-sleep 1
-
 touch ./public/css/style.css
-
-sleep 1
-
 touch ./src/sass/{app.scss,_variables.scss,_fonts.scss}
-
-sleep 1
-
 touch ./src/sass/modules/{_style.scss,_header.scss,_footer.scss,_section1.scss,_mixins.scss}
 
+sleep 1
+
+npm init -y
+
+sleep 1
+
+npm install sass
